31577a8b69ea262b75812600c6a4db086ea12a46
HJX <975464817@qq.com>
2016-04-24 09:35:44 +0800

    重构xmlname

--- src/main/java/com/example/administrator/androidmvp/view/MainActivity.java
+++ src/main/java/com/example/administrator/androidmvp/view/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends Activity implements View.OnClickListener, IMai
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
 
         initView();
     }
